f5f67a659571109eeb6a3c4d76557305d3545810
Olexandrd <olexandrd@gmail.com>
2024-05-25 21:27:47 +0300

    Task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartButtonClick() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
